74fcac172181e8effcbbc8543052d3b4e7f818c4
finotellicarlos <carlos.finotelli@outlook.com>
2023-10-23 16:51:06 -0300

    feat: add persistence to products in cart

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -83,15 +83,29 @@ const Header = () => {
               </Button>
             )}
 
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <HomeIcon size={16} />
-              Início
-            </Button>
+            <SheetClose asChild>
+              <Link href="/">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <HomeIcon size={16} />
+                  Início
+                </Button>
+              </Link>
+            </SheetClose>
 
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <PercentIcon size={16} />
-              Ofertas
-            </Button>
+            <SheetClose asChild>
+              <Link href="/">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <PercentIcon size={16} />
+                  Ofertas
+                </Button>
+              </Link>
+            </SheetClose>
 
             <SheetClose asChild>
               <Link href="/catalog">
@@ -106,14 +120,16 @@ const Header = () => {
             </SheetClose>
 
             {status === "authenticated" && (
-              <Button
-                onClick={handleLogoutClick}
-                variant="destructive"
-                className="w-full justify-start gap-2"
-              >
-                <LogOutIcon size={16} />
-                Sair
-              </Button>
+              <SheetClose asChild>
+                <Button
+                  onClick={handleLogoutClick}
+                  variant="destructive"
+                  className="w-full justify-start gap-2"
+                >
+                  <LogOutIcon size={16} />
+                  Sair
+                </Button>
+              </SheetClose>
             )}
           </div>
         </SheetContent>
