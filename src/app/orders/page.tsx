40ba80c7a796eb2400d88a2f7b7714fc26717064
finotellicarlos <carlos.finotelli@outlook.com>
2023-10-24 15:48:39 -0300

    fix layout orders to desktop

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -28,7 +28,7 @@ async function OrderPage() {
   });
 
   return (
-    <div className="p-5">
+    <div className="container p-5">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant="outline"
